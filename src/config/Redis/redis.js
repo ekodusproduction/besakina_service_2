b937fd152736fe51b0dfc7dc4557f8208300f2a6
jay-ekodus <jay.sardar@zycelltech.com>
2024-11-23 12:52:41 +0530

    added redis

--- src/config/Redis/redis.js
+++ src/config/Redis/redis.js
@@ -8,7 +8,7 @@ const redisHost = process.env.REDIS_HOST || '127.0.0.1';
 const redisPort = process.env.REDIS_PORT || 6379;
 const redisPassword = process.env.REDIS_PASSWORD;
 
-const redis = new Redis({
+const config = {
     host: redisHost,
     port: redisPort,
     reconnectOnError: (err) => {
@@ -23,27 +23,57 @@ const redis = new Redis({
         console.log(`Retrying Redis connection: attempt ${times}, delay ${delay}ms`);
         return delay;
     },
-});
+}
 
+let redis
 // Function to log connection status
+const connectRedis = async function () {
+    try {
+        redis = new Redis(config);
+        redis.on('connect', async () => {
+            console.log('Redis client connected');
 
+        });
+        redis.on('error', (err) => {
+            console.error('Redis Client Error:', err);
+        });
+
+        redis.on('close', () => {
+            console.log('Redis client disconnected');
+        });
+        redis.on('ready', async () => {
+
+            console.log('Redis client Redis (Inside mongoose)');
+            // Call data loading functions once Redis is connected
+            console.log(`Redis category loading job executed at ${new Date().toISOString()}`);
+            const categoryCount = await categoryListLoader();
+            console.log(`Number of categories loaded: ${categoryCount}`);
+
+            const categorySchemaCount = await categorySchemaLoader();
+            console.log(`Number of category schema loaded: ${categorySchemaCount}`);
+
+            const categoryTagsCount = await categoryTagsLoader();
+            console.log(`Number of category tags loaded: ${categoryTagsCount}`);
+
+            console.log(`Redis advertisement loading job executed at ${new Date().toISOString()}`);
+
+            const advertisementCount = await advertisementListLoader();
+            console.log(`Number of advertisements loaded: ${advertisementCount}`);
+
+            const advertisementHashCount = await advertisementHashLoader();
+            console.log(`Number of advertisement hashes loaded: ${advertisementHashCount}`);
+
+        })
+        redis.on('reconnecting', () => {
+            console.log('Reconnecting to Redis...');
+        });
+    } catch (err) {
+        console.error('Redis Error:', err);
+    }
+}
 // Call loaders when Redis is connected
 
-redis.on('connect', async () => {
-    console.log('Redis client connected');
 
-});
-redis.on('error', (err) => {
-    console.error('Redis Client Error:', err);
-});
-
-redis.on('close', () => {
-    console.log('Redis client disconnected');
-});
-
-redis.on('reconnecting', () => {
-    console.log('Reconnecting to Redis...');
-});
 
 // Export redis client and connectRedis function
-export { redis }
+export { redis, connectRedis }
--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from "mongoose";
-import { redis } from './Redis/redis.js'
+import { connectRedis } from './Redis/redis.js'
 
 import {
     categoryListLoader,
@@ -24,29 +24,7 @@ export const mongooseConnection = async function () {
         const collections = await db.listCollections().toArray();
         console.log('Connected to MongoDB using Mongoose and reindexed all collections');
 
-        redis.on('ready', async () => {
-
-            console.log('Redis client Redis (Inside mongoose)');
-            // Call data loading functions once Redis is connected
-            console.log(`Redis category loading job executed at ${new Date().toISOString()}`);
-            const categoryCount = await categoryListLoader();
-            console.log(`Number of categories loaded: ${categoryCount}`);
-
-            const categorySchemaCount = await categorySchemaLoader();
-            console.log(`Number of category schema loaded: ${categorySchemaCount}`);
-
-            const categoryTagsCount = await categoryTagsLoader();
-            console.log(`Number of category tags loaded: ${categoryTagsCount}`);
-
-            console.log(`Redis advertisement loading job executed at ${new Date().toISOString()}`);
-
-            const advertisementCount = await advertisementListLoader();
-            console.log(`Number of advertisements loaded: ${advertisementCount}`);
-
-            const advertisementHashCount = await advertisementHashLoader();
-            console.log(`Number of advertisement hashes loaded: ${advertisementHashCount}`);
-
-        })
+        await connectRedis()
 
     } catch (err) {
         console.error('Failed to connect to MongoDB using Mongoose', err);
