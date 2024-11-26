6ba156d63d4173a97cabc5cd132c871ed2e0176c
jay-ekodus <jay.sardar@zycelltech.com>
2024-11-26 11:37:23 +0530

    added redis

--- src/Features/Advertisement/BaseModel/base.model.js
+++ src/Features/Advertisement/BaseModel/base.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const commonOptions = {
-    discriminatorKey: 'categoryId',
+    // discriminatorKey: 'categoryId',
     collection: 'advertisement',
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
 };
