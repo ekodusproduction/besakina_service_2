cfbb68b503f59afad21d59e8dd94c781b0d35cc0
jay-ekodus <jay.sardar@zycelltech.com>
2024-11-26 11:24:59 +0530

    added redis

--- src/Middlewares/advCategory.middleware.js
+++ src/Middlewares/advCategory.middleware.js
@@ -13,6 +13,7 @@ export const advCategoryValidationMiddleware = async (req, res, next) => {
         // Check if the category exists and is active
         // const category = await checkCategoryById(req.body.categoryId)
         const category = await getDB().collection("categories").find({ _id: categoryId });
+        console.log("category", category)
         if (!category) {
             return sendError(res, 'Invalid or inactive categoryId.', 404);
         }
