77e0ffdfda8f61e7e298d412cd07aad26156bf0f
jay-ekodus <jay.sardar@zycelltech.com>
2024-11-28 15:00:40 +0530

    added redis

--- src/Features/Advertisement/base.controller.js
+++ src/Features/Advertisement/base.controller.js
@@ -49,8 +49,7 @@ export const getListAdvertisement = async (req, res, next) => {
 
 export const filterAdvertisement = async (req, res, next, Model) => {
     try {
-        const { categoryId } = req.params;
-        const { search } = req.query;
+        const {categoryId, search } = req.query;
         const result = await repository.filterAdvertisement(categoryId, JSON.parse(search));
         if (result.error) {
             return await sendError(res, result.message, result.statusCode, result.data)
